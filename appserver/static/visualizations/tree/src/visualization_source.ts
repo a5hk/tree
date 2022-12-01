a720101634e73051434839b9ec7695a170cf72f3
a5hk <5412540+a5hk@users.noreply.github.com>
2022-12-01 22:05:26 +0000

    correct widths

--- appserver/static/visualizations/tree/src/visualization_source.ts
+++ appserver/static/visualizations/tree/src/visualization_source.ts
@@ -25,8 +25,9 @@ define([
     initialize: function () {
       this.chunk = 50000;
       this.offset = 0;
-      // SplunkVisualizationBase.prototype.initialize.apply(this, arguments);
+      SplunkVisualizationBase.prototype.initialize.apply(this, arguments);
       // Initialization logic goes here
+      this.el.classList.add("tree-container");
     },
 
     // Optionally implement to format data returned from search.
@@ -52,49 +53,23 @@ define([
       // | makeresults | eval s= "p1,p2,12-p1,p3-p4,p5-p3,p4-p5,p6-p3,p7-p8,p9-p9,p10-p8,p11"| eval s=split(s, "-") | mvexpand s|eval s=split(s, ",")|eval parent=mvindex(s,0), child=mvindex(s,1), value=mvindex(s,2)|table parent, child, value
       let inputData: BranchPair[] = data.rows;
       let f = new Forest(inputData);
-      // let myChart = echarts.init(this.el);
       let series = f.toSeries();
 
+      // create all elements first to get correct element sizes, then initialize charts
       for (const t in f.trees) {
         if (!document.getElementById(`tree-child-${t}`)) {
           let div = document.createElement("div");
           div.setAttribute("id", `tree-child-${t}`);
-          div.style.height = "400px";
           this.el.appendChild(div);
-
-          let option = {
-            tooltip: {
-              trigger: "item",
-              triggerOn: "mousemove",
-            },
-            series: series[0],
-          };
-          console.log(series.shift());
-          console.log(option);
-
-          let myChart = echarts.init(div);
-          myChart.setOption(option);
         }
       }
 
-      // let divs = this.el.querySelectorAll('[id^=tree-child-]')
+      let divs = this.el.querySelectorAll("[id^=tree-child-]");
 
-      // for (const d in divs) {
-      //   let treeChart = echarts.init(d)
-      //   treeChart.setOption({series: })
-      // }
-
-      // let option;
-      // option = {
-      //   tooltip: {
-      //     trigger: "item",
-      //     triggerOn: "mousemove",
-      //   },
-      //   series: f.toSeries(),
-      // };
-
-      // myChart.hideLoading();
-      // myChart.setOption(option);
+      for (const d of divs) {
+        let treeChart = echarts.init(d);
+        treeChart.setOption({ series: series.filter((s) => d.id.replace("tree-child-", "") == s.name) });
+      }
 
       this.offset += data.rows.length;
       this.updateDataParams({ count: this.chunk, offset: this.offset });
@@ -119,10 +94,10 @@ class Serie {
   type = "tree";
   name = "Tree";
   data: Branch[];
-  // top = "5%";
-  // left = "5%";
-  // bottom = "5%";
-  // right = "5%";
+  top = "4";
+  left = "8";
+  bottom = "4";
+  right = "8";
   symbolSize = 7;
   label = {
     position: "left",
@@ -198,6 +173,14 @@ class Forest {
     }
     return series;
   }
+
+  // toSerie(t: string): Serie {
+  //   if (this.trees[t]) {
+  //     let s = new Serie(this.trees[t]);
+  //     s.name = t;
+  //     return s;
+  //   }
+  // }
 
   toJSON(): string {
     return JSON.stringify(this.trees);
